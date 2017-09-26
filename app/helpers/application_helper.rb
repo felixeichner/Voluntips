ad5b425a3c9747764871db664f4d1b05f6b60ce2
felixeichner <eichner.f@googlemail.com>
2017-09-26 17:35:57 +0200

    added admin_links helper and created buttons for admin links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def admin_links(object, edit_path)
+		(link_to 'Bearbeiten', edit_path, class: "btn btn-sm btn-warning") + ' ' +
+		(link_to 'Löschen', object,
+												method: :delete,
+												data: { confirm: 'Are you sure?' }, class: "btn btn-sm btn-warning")
+	end
+
 end
