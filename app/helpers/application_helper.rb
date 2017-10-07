3d64159f14b05e3b6453a80c879417496e23bf4a
felixeichner <eichner.f@googlemail.com>
2017-10-07 08:58:15 +0200

    added all projects button and helper method for filter buttons, added margin bottom to voluntext-container

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,11 @@ module ApplicationHelper
 	
 	end
 
+	def filter_button_helper(label, terrain)
+		link_to label, projects_path('', :terrain => terrain),
+						class: "btn btn-designed btn-filter #{active?(@session_params['terrain'], terrain)}"
+	end
+
   def active?(key, value)
   	"active" if key == value
   end
