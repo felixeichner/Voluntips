e78815b651be3faabf72cb39ccbab4d7cb512c59
felixeichner <eichner.f@googlemail.com>
2017-10-10 21:08:00 +0200

    added google analytics script and sitemap

---
+++ config/sitemap.rb
@@ -0,0 +1,46 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://voluntips.herokuapp.com"
+SitemapGenerator::Sitemap.compress = false
+
+SitemapGenerator::Sitemap.create do
+
+  add root_path
+
+  add projects_path
+
+  Project.find_each do |project|
+    add project_path(project)
+  end
+
+  VolunText.find_each do |volun_text|
+    add volun_text_path(volun_text)
+  end
+
+  add contact_path
+
+  add datenschutz_path
+  add disclaimer_path
+  add impressum_path
+
+
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
+end
